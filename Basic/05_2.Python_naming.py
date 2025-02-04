c5d36438185028aac27845b308af28978597423d
ishaat-dipu <ishaat48@gmail.com>
2025-02-04 17:23:33 +0600

    naming2

--- Basic/05_2.Python_naming.py
+++ Basic/05_2.Python_naming.py
@@ -5,3 +5,26 @@ _gg_ = 20
 gs_t = 30
 rt1 = (_gg_ + gs_t)
 print(rt1)
+
+
+
+## Extra ##
+
+# Here are Example of Multi Word Variable Names
+
+"""Variable Name With More Than One word can be difficult to read
+There are Several Techniques you can use to make them more Readable
+"""
+
+#Camel Case : Each word Except the First will start a capital
+myVariableName = "Ishaat"
+
+#Pascal case : Each Word start With a Capital Letter 
+MyvariableName = "john"
+
+#Snake Case : Each Word Will be Seperate by an Under Score  
+my_vsriale_name = "Didu"
+
+print (myVariableName)
+print(MyvariableName)
+print(my_vsriale_name)
