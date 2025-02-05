e88b64ab17b0887d81efccc299377f1a8b03f0ca
ishaat-dipu <ishaat48@gmail.com>
2025-02-05 14:31:32 +0600

    Multiple variable Assign

---
+++ Basic/05_3.Multiple_value_assign.py
@@ -0,0 +1,16 @@
+#One variable multiple assign
+fruits = "Apple", "Mango", "Jackfruit"
+print(fruits)
+
+#On assign - Multiple Variable
+x = y = z = "fruits"
+print(x)
+print(y)
+print(z)
+
+# Unpacking The Tupple 
+fruits = ["banana", "carrot", "jackfruit"]
+x= y= z = fruits 
+print(x)
+print(y)
+print(z)
