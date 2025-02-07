0c8ff626d173965c6dfeb9fcc1f3d3180561912b
ishaat-dipu <ishaat48@gmail.com>
2025-02-07 06:59:16 +0600

    output variable

---
+++ Basic/05_4.Output_variale.py
@@ -0,0 +1,13 @@
+#We often Tell That the Output variale is print
+print ("Hello World")
+## Hear print is an out put variable 
+
+# we can use print() function , it can output multiple variales, seperate by comma
+x = "Python"
+y = "is"
+z = "Awesome "
+
+print(x,y,z)
+
+# we can also concatanate sentence using + operator
+print(x+y+z)
