78e9438927a7d304447982403019cbe95eb166eb
ishaat-dipu <ishaat48@gmail.com>
2025-02-12 04:16:27 +0600

    Data-Types

---
+++ Basic/06.Data_Types.py
@@ -0,0 +1,47 @@
+# Data Type is Set when You assign a value to the Variable
+# For an Example Suppose:
+
+# Integer
+x = 2
+print(type(x))  # <class 'int'>
+
+# Float
+y = 3.14
+print(type(y))  # <class 'float'>
+
+# String
+z = "Hello"
+print(type(z))  # <class 'str'>
+
+# List
+a = [1, 2, 3]
+print(type(a))  # <class 'list'>
+
+# Tuple
+b = (1, 2, 3)
+print(type(b))  # <class 'tuple'>
+
+# Dictionary
+c = {"key": "value"}
+print(type(c))  # <class 'dict'>
+
+# Set
+d = {1, 2, 3}
+print(type(d))  # <class 'set'>
+
+# Boolean
+e = True
+print(type(e))  # <class 'bool'>
+
+
+#Same Just Like this We can use
+a = "Hello world"
+b = 20.5
+c = 1j
+d = ["apple", "banana", "cherry"]
+e =("apple", "banana", "cherry")
+f = range(6)
+g = {"name": "John", "age": 30, "city": "New York" }
+h = True
+i = bytearray(4)
+print(type (x), (a), (b), (c), (d), (e), (f), (g), (h))
