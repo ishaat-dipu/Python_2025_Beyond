7cd355b75d7a69ba78a322994ffa27e49b9e719e
ishaat-dipu <ishaat48@gmail.com>
2025-02-13 02:59:38 +0600

    numbers type convert

---
+++ Basic/07.Numbers_type_conv.py
@@ -0,0 +1,18 @@
+#Number and Type Conversion
+x=2
+Y =3.03
+Z = 1J
+
+print (type(x))
+
+# To Verify Type of Any Object python Use The Type() predefined Function
+
+# we can do "Type Conversion" by Using the Built-in Functions
+
+a =3
+b=7.5
+c= 1+2j
+p= int(b)
+q= float(a)
+r = complex(c)
+print (p,q,r)
