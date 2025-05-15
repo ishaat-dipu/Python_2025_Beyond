c6abbeb3d79e96d4beba6716d8399b68c8d7f90a
Ishaat Rahman Dipu <ishaat48@gmail.com>
2025-05-15 07:23:18 +0600

    Update 08_1.Python_string.py

---
+++ Basic/08_1.Python_string.py
@@ -0,0 +1,43 @@
+#String in python are surrounded by either single or Double quotes marks.
+#'Hello' is the same as "Hello"
+#You can display strings with the print() function:
+###Example
+print("Hello")
+print('Hello')
+
+# We can also use quotes Inside Quotes
+#Example
+print("he is called 'Ishaat'")
+print('he is called "Ishaat"')
+# We can also use triple quotes
+#Example
+print('''Hello
+World''')
+print("""Hello
+World""")
+
+#Assign String to a Variable
+a = "Ishaat"
+print(a)
+
+#Assign String into Multi Strings
+a = """Ishaat
+Ishaat"""
+
+
+
+# Check if Strings are Arrays 
+a = "Ishaat"
+print(a[0]) 
+print(a[1])
+
+
+# Looping Through String
+#Example
+for x in "Ishaat":
+    print(x)
+    
+#String Length
+#Example
+a = "Ishaat"
+print (len())
