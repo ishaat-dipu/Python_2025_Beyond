83e33dfa0fb5695e21665f9dd2dae6d3279f2d52
ishaat-dipu <ishaat48@gmail.com>
2025-05-15 07:29:22 +0600

    Next push

---
+++ Basic/08_2.check_String.py
@@ -0,0 +1,17 @@
+txt = " The Best things in Life are free!"
+print ("Life" in txt)
+
+# Print Only if "Free" is Present :
+txt = " The Best things in Life are free!"
+if "free" in txt:
+    print("Yes, 'Free' is present.")
+
+# Check if "expensive" is NOT present in the text:
+txt = " The Best things in Life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+
+    # Check if "expensive" is NOT present in the text:
+txt = " The Best things in Life are free!"
+if "free" not in txt:
+    print("No, 'expensive' is NOT present.")
