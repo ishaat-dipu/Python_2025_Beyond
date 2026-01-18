db1571ca95331f44bd45000bc643483c6356bbc7
ishaat-dipu <ishaat48@gmail.com>
2026-01-17 19:53:19 -0500

    New_code5

---
+++ Basic/15.Multiple_function_Arguments.py
@@ -0,0 +1,8 @@
+#Ask user For Their Name
+name = input ("what is your Name")
+#Say Hello to user 
+# We can use argument multiple way like 
+print("hello")
+print(name)
+#Or We can use it by concatanating the Two variable in one print Function
+print("Hello" + "" + name)
