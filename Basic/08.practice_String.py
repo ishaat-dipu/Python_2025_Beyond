2b495d6e0e81440702e4743e8d04bd3651c40feb
ishaat-dipu <ishaat48@gmail.com>
2026-06-28 18:53:24 +0600

    28.06.2026

--- Basic/08.practice_String.py
+++ Basic/08.practice_String.py
@@ -1,27 +1,31 @@
-"""messy_comment = "   i lOve pYtHoN. pYtHoN Is ThE bEsT!   "
-Your Task:
+# """messy_comment = "   i lOve pYtHoN. pYtHoN Is ThE bEsT!   "
+# Your Task:
 
-Remove the extra whitespace at the beginning and end.
+# Remove the extra whitespace at the beginning and end.
 
-Convert the entire string to lowercase.
+# Convert the entire string to lowercase.
 
-Replace the word "python" with "coding".
+# Replace the word "python" with "coding".
 
-Use an f-string to print a message like: User commented: "[your clean string]"
+# Use an f-string to print a message like: User commented: "[your clean string]"
 
-Expected Output:"""
+# Expected Output:"""
 
-messy_comment =    "i lOve pYtHoN. pYtHoN Is ThE bEsT!   "
+# messy_comment =    "i lOve pYtHoN. pYtHoN Is ThE bEsT!   "
 
-# Remove extra white space at the begnning and end
-print(messy_comment.strip())
+# # Remove extra white space at the begnning and end
+# print(messy_comment.strip())
 
-#Convert the entire string to lowercase 
-print(messy_comment.lower())
+# #Convert the entire string to lowercase 
+# print(messy_comment.lower())
 
-#Replace the word python with coding
-print(messy_comment.replace("pYtHoN", "coding"))
+# #Replace the word python with coding
+# print(messy_comment.replace("pYtHoN", "coding"))
 
-# use an f-string to print a message
-a = 57
-print(f"{a} is our call ")
+# # use an f-string to print a message
+# a = 57
+# print(f"{a} is our call ")
+
+# use an f-string to add a float and test it out if it works or not 
+a = 43.23
+print(f"{a} is our target to check if it shows the float number or not")
--- Basic/09.Python_Input.py
+++ Basic/09.Python_Input.py
@@ -1,2 +1,17 @@
+"""1.Input Function"""
 name = input("Enter your name: ")
 print("Hello " + name)
+
+"""2. The Type Trap (Why int() is necessary)"""
+# Bad Example
+age1 = input("Enter your age: ")
+print("My age is " + age1) # but ultimately it is returning the number as string not an actual digit
+# To get the actual digit we use int(input()) -- it locks in to give the input of integer
+age2 = int(input("Enter your real age"))
+print(f"My real age is {age2}")
+
+# Task - small
+# Write a program: ask name & age, print a greeting
+name1 = input("Enter your name")
+age3= int(input("how old are you"))
+print(f"Greeting {name1} you are eligible to enter as your age is {age3}")
